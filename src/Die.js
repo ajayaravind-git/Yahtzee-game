ec53309d19ba6b1ad2f6546960323e717deaf849
Ajay Aravind <ajayaravind.me@gmail.com>
2022-12-07 15:21:38 +0530

    fixed the dice click

--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./Die.css";
 
+
 class Die extends Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleClick = this.handleToggleClick.bind(this)
+
+  }
+
+  handleToggleClick() {
+    this.props.handleClick(this.props.idx)
+
+  }
   render() {
     return (
       <button
         className={"Die"}
         style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        onClick={this.handleToggleClick}
       >
         {this.props.val}
       </button>
